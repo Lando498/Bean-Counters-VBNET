7b919113ff16d1499376265a45e3fc2668050d1f
Lewis Orr Houston <wllhouston@glow.sch.uk>
2025-11-13 19:27:43 +0000

    Add project files.

---
+++ WinFormsApp5/Form1.Designer.vb
@@ -0,0 +1,123 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        components = New ComponentModel.Container()
+        PictureBox1 = New PictureBox()
+        PictureBox2 = New PictureBox()
+        Timer1 = New Timer(components)
+        Timer2 = New Timer(components)
+        Label1 = New Label()
+        Timer3 = New Timer(components)
+        Label2 = New Label()
+        CType(PictureBox1, ComponentModel.ISupportInitialize).BeginInit()
+        CType(PictureBox2, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' PictureBox1
+        ' 
+        PictureBox1.BackColor = Color.Transparent
+        PictureBox1.Image = My.Resources.Resources._1
+        PictureBox1.Location = New Point(279, 470)
+        PictureBox1.Name = "PictureBox1"
+        PictureBox1.Size = New Size(203, 186)
+        PictureBox1.SizeMode = PictureBoxSizeMode.AutoSize
+        PictureBox1.TabIndex = 0
+        PictureBox1.TabStop = False
+        ' 
+        ' PictureBox2
+        ' 
+        PictureBox2.BackColor = Color.Transparent
+        PictureBox2.Image = My.Resources.Resources._11
+        PictureBox2.Location = New Point(329, -101)
+        PictureBox2.Name = "PictureBox2"
+        PictureBox2.Size = New Size(105, 112)
+        PictureBox2.SizeMode = PictureBoxSizeMode.AutoSize
+        PictureBox2.TabIndex = 1
+        PictureBox2.TabStop = False
+        ' 
+        ' Timer1
+        ' 
+        Timer1.Enabled = True
+        ' 
+        ' Timer2
+        ' 
+        Timer2.Enabled = True
+        Timer2.Interval = 50
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.BackColor = Color.Transparent
+        Label1.Font = New Font("Segoe UI", 20F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label1.Location = New Point(620, 31)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(95, 37)
+        Label1.TabIndex = 2
+        Label1.Text = "Score:"
+        ' 
+        ' Timer3
+        ' 
+        Timer3.Enabled = True
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.BackColor = Color.Transparent
+        Label2.Font = New Font("Segoe UI", 48F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label2.Location = New Point(171, 274)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(409, 86)
+        Label2.TabIndex = 3
+        Label2.Text = "GAME OVER"
+        Label2.Visible = False
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackgroundImage = My.Resources.Resources._13
+        BackgroundImageLayout = ImageLayout.Stretch
+        ClientSize = New Size(734, 650)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Controls.Add(PictureBox2)
+        Controls.Add(PictureBox1)
+        DoubleBuffered = True
+        Name = "Form1"
+        Text = "Form1"
+        CType(PictureBox1, ComponentModel.ISupportInitialize).EndInit()
+        CType(PictureBox2, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents Timer2 As Timer
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Timer3 As Timer
+    Friend WithEvents Label2 As Label
+
+End Class
---
+++ WinFormsApp5/Form1.vb
@@ -0,0 +1,101 @@
+﻿Imports System
+Imports System.Drawing
+Imports System.Windows.Forms
+Public Class Form1
+    Dim num As Integer
+    Dim VelocityY As Double = 0
+    Dim gravity As Double = 0.5
+    Dim r As New Random()
+    Dim Score As Integer = 0
+    Dim groundLevel As Integer
+    Dim X As Integer
+
+    Private Sub Form1_MouseMove(Sender As Object, e As MouseEventArgs) Handles Me.MouseMove
+        PictureBox1.Location = New Point(e.X - 90, 466)
+    End Sub
+
+    Public Sub New()
+        InitializeComponent()
+        Me.DoubleBuffered = True
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Label2.Visible = False
+        PictureBox2.BringToFront()
+        num = r.Next(1, 6)
+        groundLevel = Me.ClientSize.Height - PictureBox2.Height
+        Timer1.Interval = 20
+        Timer1.Start()
+        Timer2.Start()
+        Label1.Text = "Score: 0"
+    End Sub
+
+    Private Sub Timer2_Tick(sender As Object, e As EventArgs) Handles Timer2.Tick
+        VelocityY += gravity
+        PictureBox2.Top += CInt(VelocityY)
+        If PictureBox2.Bounds.IntersectsWith(PictureBox1.Bounds) Then
+            Score += 1
+            Label1.Text = "Score: " & Score.ToString()
+            PictureBox2.Top = -64
+            num = r.Next(1, 6)
+            Select Case num
+                Case 1
+                    PictureBox2.Location = New Point(12, -104)
+                Case 2
+                    PictureBox2.Location = New Point(162, -104)
+                Case 3
+                    PictureBox2.Location = New Point(312, -104)
+                Case 4
+                    PictureBox2.Location = New Point(462, -104)
+                Case 5
+                    PictureBox2.Location = New Point(612, -104)
+            End Select
+            VelocityY = 0
+        End If
+        If PictureBox2.Top >= groundLevel Then
+            PictureBox2.Top = groundLevel
+            VelocityY = 0
+
+            num = r.Next(1, 6)
+        End If
+        X = PictureBox1.Left
+        If X < -37 Then
+            PictureBox1.Location = New Point(-37, 466)
+            Cursor.Position = New Point(60, 300)
+        End If
+        If X > 575 Then
+            PictureBox1.Location = New Point(575, 466)
+            Cursor.Position = New Point(640, 300)
+
+        End If
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        If PictureBox2.Top = groundLevel Then
+            Score -= 1
+            Label1.Text = "Score: " & Score.ToString()
+            Select Case num
+                Case 1
+                    PictureBox2.Location = New Point(12, -104)
+                Case 2
+                    PictureBox2.Location = New Point(162, -104)
+                Case 3
+                    PictureBox2.Location = New Point(312, -104)
+                Case 4
+                    PictureBox2.Location = New Point(462, -104)
+                Case 5
+                    PictureBox2.Location = New Point(612, -104)
+            End Select
+
+        End If
+    End Sub
+
+    Private Sub Timer3_Tick(sender As Object, e As EventArgs) Handles Timer3.Tick
+        If Score < 0 Then
+            Label2.Visible = True
+            Application.DoEvents()
+            Threading.Thread.Sleep(1500)
+            End
+        End If
+    End Sub
+End Class
